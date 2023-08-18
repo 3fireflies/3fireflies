331d72d278d263a82de696fa0b3ff34ac9fdd93d
max.weber <max.weber@meso.design>
2023-08-18 19:21:04 +0200

    alert

--- ems/src/main.js
+++ ems/src/main.js
@@ -1,5 +1,7 @@
 import { Application } from '@splinetool/runtime';
 
+alert('aktiviere den Ton für ein optimales Nutzungserlebnis.');
+
 const canvas = document.getElementById('canvas3d');
 const app = new Application(canvas);
 app.load('https://prod.spline.design/i3JKZHxaqprBW1YI/scene.splinecode');
